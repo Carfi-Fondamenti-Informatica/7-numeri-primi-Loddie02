642d1af6aeba1f6793fb14b44130de0eedc9c46e
Loddie02 <91880336+Loddie02@users.noreply.github.com>
2021-12-01 23:59:21 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,5 +1,15 @@
+using namespace std;
 #include <iostream>
+#include "lib.h"
 
-int main(){
-  return 0;
+
+int main() {
+    int numero;
+    cin>>numero;
+    if(isNumeroPrimo(numero,2)) {
+        cout<<"numero primo";
+    }else{
+        cout<<"numero non primo";
+    }
+    return 0;
 }
